7828c740020aa2b529ab71057ab3250ce58c980d
BruhTheyWontLetMePutMyNameFinchat <67127651+BruhTheyWontLetMePutMyNameFinchat@users.noreply.github.com>
2020-09-29 20:42:00 -0400

    Update yeet.cs

--- yeet.cs
+++ yeet.cs
@@ -1,3 +1,4 @@
+
 using System;
 
 namespace Coding_stuff
@@ -6,28 +7,40 @@ namespace Coding_stuff
     {
         static void Main(string[] args)
         {
-  
-            Random numberGen = new Random();
-            int number = numberGen.Next(1,11);
-            Console.WriteLine("Put in number 1-10");
-            int randomnumber = 1000;
-            while(randomnumber != number)
-            {
+
+        Random numberGen = new Random();
+        int number = numberGen.Next(1,11);
+        Console.WriteLine("Put in number 1-10");
+        int randomnumber = 1000;
+        int attempts = 0;
+        while(randomnumber != number)
+        {   attempts++;
             int guess = Convert.ToInt32(Console.ReadLine());
             if(guess == number)
             {
-                Console.WriteLine("Nice");
-                break;
+            Console.WriteLine("Nice");
+            break;        
             }
-            else if (guess > number)
-            {
+                else if (guess > number)
+                {
                 Console.WriteLine("Guess lower");
-            }
-            else if (guess < number)
-            {
+                }
+                else if (guess < number)
+                {
                 Console.WriteLine("Guess higher");
+                }
+               
+        }
+        
+            if(attempts <= 5)
+            {
+            Console.WriteLine($"It only took you {attempts} to find the right number, poggers.");
             }
+            else if(attempts > 5)
+            {
+            Console.WriteLine($"Hahahahahaha, it took you {attempts} to find number, nice.");
             }
+        
         }
     }
-}
+} 
